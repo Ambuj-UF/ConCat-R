abf1c3008d5f9e3405e9fe7d42889c36d9620a46
Ambuj-UF <sendambuj@gmail.com>
2014-10-22 03:53:45 -0400

    dev

--- ConCat-R.R
+++ ConCat-R.R
@@ -23,8 +23,6 @@
 
 library(seqinr)
 
-files = list.files(pattern = '*.fas')
-
 bwt <- function(s) {
     """Apply Burrows-Wheeler transform to input string."""
     
@@ -57,72 +55,75 @@ insertRow <- function(existingDF, newrow, r) {
     return(existingDF)
 }
 
-dataObject = data.frame('Test','Test','Test')
-header_col = c('FileName', 'Species', 'Sequence')
-names(dataObject) = header_col
 
-for (file in files) {
-    data = read.alignment(file, "fasta")
-    x = 1
-    while (x <= length(data$seq)){
-        dataObject = insertRow(dataObject, data.frame("FileName" = file, "Species" = data$nam[x], "Sequence" = data$seq[x]), 1)
-        x = x + 1
-    }
-}
+ConCat <- function(dataFileExtension){
+    files = list.files(pattern=dataFileExtension)
+    dataObject = data.frame('Test','Test','Test')
+    header_col = c('FileName', 'Species', 'Sequence')
+    names(dataObject) = header_col
 
-newDataObject = new.env()
-
-for (filename in files){
-    newDataObject[[filename]] = data.frame('Test','Test','Test')
-}
-
-for (filename in files){
-    for (data in dataObject){
-        if (data$FileName == filename){
-            newDataObject[[filename]] = insertRow(newDataObject[[filename]], data, 1)
-    }
-}
-    
-row_to_find <- data.frame(b="cat")
-
-speciesAll = unique(dataObject$Species)
-
-for (filename in files){
-    for (species in speciesAll){
-        row_to_find = data.frame("Species"=species)
-        if (isTRUE(nrow(merge(row_to_find,newDataObject[filename]))>0) == FALSE){
-            newDataObject[[filename]] = insertRow(newDataObject[[filename]], \
-                data.frame("FileName" = filename, "Species" = species, "Sequence" = paste(rep(strsplit("?","")[[1]],\
-                    each=length(newDataObject[[filename]]$Sequence[1])), collapse="")), 1)
+    for (file in files) {
+        data = read.alignment(file, "fasta")
+        x = 1
+        while (x <= length(data$seq)){
+            dataObject = insertRow(dataObject, data.frame("FileName" = file, "Species" = data$nam[x], "Sequence" = data$seq[x]), 1)
+            x = x + 1
         }
     }
-}
 
-seqObject = c()
-concatFrame = data.frame('Test','Test','Test')
-    
-for (sname in speciesAll){
+    newDataObject = new.env()
+
     for (filename in files){
-        for (data in newDataObject[[filename]]){
-            if (data[2] == sname){
-                seqObject = c(seqObject, data[3])
+        newDataObject[[filename]] = data.frame('Test','Test','Test')
+    }
+
+    for (filename in files){
+        for (data in dataObject){
+            if (data$FileName == filename){
+                newDataObject[[filename]] = insertRow(newDataObject[[filename]], data, 1)
             }
         }
     }
-    concatFrame = insertRow(concatFrame, data.frame("Filename" = "MasterConcat", "Species" = sname,\
-        "Sequence" = do.call(paste, c(as.list(sdata), sep=""))), 1)
-}
+    
+    row_to_find <- data.frame(b="cat")
 
+    speciesAll = unique(dataObject$Species)
 
-fileConn<-file("output.fasta")
-for (data in concatFrame){
-    writeLines(c(">",data$Species), fileConn)
-    writeLines(c(data$Sequence), fileConn)
-    writeLines(c("\n\n"), fileConn)
-}
+    for (filename in files){
+        for (species in speciesAll){
+            row_to_find = data.frame("Species"=species)
+            if (isTRUE(nrow(merge(row_to_find,newDataObject[filename]))>0) == FALSE){
+                newDataObject[[filename]] = insertRow(newDataObject[[filename]], \
+                    data.frame("FileName" = filename, "Species" = species, "Sequence" = paste(rep(strsplit("?","")[[1]],\
+                        each=length(newDataObject[[filename]]$Sequence[1])), collapse="")), 1)
+            }
+        }
+    }
 
-close(fileConn)
+    seqObject = c()
+    concatFrame = data.frame('Test','Test','Test')
+    
+    for (sname in speciesAll){
+        for (filename in files){
+            for (data in newDataObject[[filename]]){
+                if (data[2] == sname){
+                    seqObject = c(seqObject, data[3])
+                }
+            }
+        }
+        concatFrame = insertRow(concatFrame, data.frame("FileName" = "MasterConcat", "Species" = sname,\
+            "Sequence" = do.call(paste, c(as.list(sdata), sep=""))), 1)
+    }
 
+    concatFrame = concatFrame[-nrow(concatFrame),]
+
+    retAlign = data.frame("nb"=length(concatFrame$Species))
+    retAlign$nam = concatFrame$Species
+    retAlign$seq = concatFrame$Sequence
+    retAlign$com = "NA"
+    
+    write.fasta(as.list(retAlign),names(retAlign),"Output.txt")
+    return(retAlign)
 
 
 
